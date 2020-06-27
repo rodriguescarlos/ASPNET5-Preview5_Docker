0c436fce8cddd87b2720187b496b92487b304464
Carlos Rodrigues <rodrigues.carlos@gmail.com>
2020-06-27 17:35:19 -0300

    disparando o build

--- SiteContagem/Startup.cs
+++ SiteContagem/Startup.cs
@@ -26,6 +26,7 @@ namespace SiteContagem
             services.AddRazorPages();
         }
 
+		
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
